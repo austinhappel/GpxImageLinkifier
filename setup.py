e1bd3808aa387382ecb499012a3a6e89716c4a44
Austin Happel <austin@austinhappel.com>
2013-07-15 23:00:42 -0700

    updated readme and setup.py

--- setup.py
+++ setup.py
@@ -8,19 +8,24 @@ setup(
     packages=[
         'GpxImageLinkifier',
     ],
-    url='http://pypi.python.org/pypi/GpxImageLinkifier/',
+    url='https://pypi.python.org/pypi/GpxImageLinkifier',
     license='LICENSE.txt',
-    description='Links GPX tracks to photographs by matching the image\
-    timestamp\ (in the EXIF data) to the timestamp in the GPX track supplied.',
+    description='Links GPX tracks to photographs by matching the image timestamp (in the EXIF data) to the timestamp in the GPX track supplied.',
     long_description=open('README.txt').read(),
     install_requires=[
         "PIL==1.1.7",
         "gpxpy==0.8.9",
         "lxml==3.2.1",
+        "pytz"
     ],
     entry_points={
         'console_scripts': [
             'gil = GpxImageLinkifier.gil:main',
         ]
-    }
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    ]
 )
