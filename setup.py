59adb7ff37336b17ef126a4cd01d80f3a1e2e9e5
Austin Happel <austin@austinhappel.com>
2013-07-15 23:04:00 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GpxImageLinkifier',
-    version='0.1.0',
+    version='0.1.1',
     author='Austin Happel',
     author_email='austin@austinhappel.com',
     packages=[
